ffd2573bac1789f1e8bd05129b48d5d42a8ce9fa
Giacomo Biancalana <giacomo.biancalana@mail.polimi.it>
2018-09-11 16:14:09 +0200

    Parse request paths, section 3.13 of the course.

--- index.js
+++ index.js
@@ -2,15 +2,38 @@
  *  Primary file for the API
  */
 
-//Dependencies
+// DEPENDENCIES
 const http = require('http');
+const url = require('url');
 
-//define what the server does (the server should respond to all requests with a string)
-var server = http.createServer((req, res) => (
-  res.end("Helloooo, new server hereee!!\n")
-));  //server is a server object
+// DEFINE WHAT THE SERVER DOES (the server should respond to all requests with a string)
+var server = http.createServer((req, res) => {
 
-//Start the server, and had it listen on port 3000
-server.listen(3000, ()=> (
+  // GET THE URL AND PARSE IT
+  var parsedUrl = url.parse(req.url, true);
+  //true is for calling the query string module, so we are using two modules at once.
+
+  // GET THE PATH
+  var path = parsedUrl.pathname;  //untrimmed path of the user request
+  var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+  // SEND THE RESPONSE
+  res.end("Helloooo, new server hereee!!\n");
+  // res.end(JSON.stringify(parsedUrl));
+
+  // LOG THE REQUEST PATH 
+  console.log("Request is received on path: " + trimmedPath + "\n");
+
+});
+//other comments on http.createServer
+//server is a server object
+/* everytime someone hits localhost:3000, the callback function with (req, res) as parameters gets called,
+ * and everytime req and res are different objects, with lots of different information.
+ * 
+ */
+
+
+// START THE (WEB?) SERVER, AND HAVE IT LISTEN ON PORT 3000
+server.listen(3000, ()=> {
   console.log("Server is listening at port 3000 . . .")
-));
+});
