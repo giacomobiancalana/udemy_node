7787a47024e1b019d7f1dbce5fd304b430fc7030
Giacomo Biancalana <giacomo.biancalana@mail.polimi.it>
2018-09-11 12:30:51 +0200

    Add comments to understand what each group of lines of code is doing.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+/*
+ *  Primary file for the API
+ */
+
+//Dependencies
 const http = require('http');
 
-//creating the server
+//define what the server does (the server should respond to all requests with a string)
 var server = http.createServer((req, res) => (
   res.end("Helloooo, new server hereee!!\n")
-));
+));  //server is a server object
 
+//Start the server, and had it listen on port 3000
 server.listen(3000, ()=> (
   console.log("Server is listening at port 3000 . . .")
 ));
